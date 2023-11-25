082abd9893cbd1df7229eee62aa1af871dd84677
wagaojr <wagnerjunior@poli.ufrj.br>
2023-11-25 20:15:05 -0300

    update cross_validation

--- Cross_Validation/cross_validation.py
+++ Cross_Validation/cross_validation.py
@@ -5,25 +5,48 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten
 from tensorflow.keras.applications import VGG16
 import numpy as np
+import pandas as pd
+from PIL import Image
 
 import os
 
 train_data_dir = '/net/tijuca/lab/users/LuisH/wagner/ic_fl/TrainData/TrainData'
+train_data_negative = '/net/tijuca/lab/users/LuisH/wagner/ic_fl/TrainData/TrainData/Negative data'
+train_data_positive = '/net/tijuca/lab/users/LuisH/wagner/ic_fl/TrainData/TrainData/Positive data'
 
-def count_images(directory):
-    total = 0
-    for root, dirs, files in os.walk(directory):
-        total += len(files)
-    return total
+imagens = []
+labels = []
 
-total_samples = count_images(train_data_dir)
-print("Total de amostras:", total_samples)
+for nome_arquivo in os.listdir(train_data_negative):
+    if nome_arquivo.endswith('.JPG'):  
+        caminho_completo = os.path.join(train_data_negative, nome_arquivo)
+        try:
+            
+            imagem = Image.open(caminho_completo)
+            array_imagem = np.array(imagem)
+            imagens.append(array_imagem)
+            labels.append(0)  
+        except Exception as e:
+            print(f"Erro ao processar {nome_arquivo}: {str(e)}")
 
-labels = os.listdir(train_data_dir)
-num_classes = len(labels)
+for nome_arquivo in os.listdir(train_data_positive):
+    if nome_arquivo.endswith('.JPG'): 
+        caminho_completo = os.path.join(train_data_positive, nome_arquivo)
+        try:
+            
+            imagem = Image.open(caminho_completo)
+            array_imagem = np.array(imagem)
+            imagens.append(array_imagem)
+            labels.append(1)
+        except Exception as e:
+            print(f"Erro ao processar {nome_arquivo}: {str(e)}")
 
-print("Labels:", labels)
-print("Número de classes:", num_classes)
+data = {'Imagem': imagens, 'Label': labels}
+df = pd.DataFrame(data)
+
+'''# Exibir as primeiras linhas do DataFrame para verificar se os dados foram carregados corretamente
+print(df.head())
+'''
 
 image_size = (224, 224)
 batch_size = 32
@@ -33,7 +56,7 @@ datagen = ImageDataGenerator(rescale=1./255)
 kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
 def create_model():
-    model = Sequential([
+    model = Sequential([         # ---------
         Flatten(input_shape=(image_size[0], image_size[1], 3)),
         Dense(128, activation='relu'),
         Dense(1, activation='sigmoid')
@@ -65,7 +88,7 @@ def load_and_preprocess_data(train_indices, val_indices):
 
 # Aplicar a validação cruzada
 results = []
-for train_indices, val_indices in kfold.split(np.zeros((total_samples,)), labels):
+for train_indices, val_indices in kfold.split(np.zeros((len(df['Imagem']),)), len(df['Label'])):    # --------------
     train_data, val_data = load_and_preprocess_data(train_indices, val_indices)
     model = create_model()
     
